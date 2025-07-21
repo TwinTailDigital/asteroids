19454e281050e3638e543793b2fba2b123bee6cf
TwinTailDigital <admincat@twintaildigital.com>
2025-07-21 20:27:29 +0200

    add player controls

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ from player import *
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
+    group_updateable = pygame.sprite.Group()
+    group_drawable = pygame.sprite.Group()
+    Player.containers = (group_updateable,group_drawable)
     game_clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
+    print("Starting Asteroids!")
     run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        group_updateable.update(dt)
+        for item in group_drawable:
+            item.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
 
