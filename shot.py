80ccaa039d7a0bbb335cc918f00205903f77cbcb
TwinTailDigital <admincat@twintaildigital.com>
2025-07-29 18:42:11 +0200

    update shot visuals and size

--- shot.py
+++ shot.py
@@ -1,13 +1,19 @@
 import pygame
+import math
+import random
 from circleshape import *
 from constants import *
 
 class Shot(CircleShape):
     def __init__(self,x,y,radius):
         super().__init__(x,y,radius)
+        self.current_radius = 0.0
+
     
     def draw(self, screen):
-        pygame.draw.circle(screen,COLOR_SHOT,self.position,self.radius,2)
+        pygame.draw.circle(screen,COLOR_SHOT,self.position,self.current_radius,2)
 
     def update(self, dt):
         self.position += self.velocity * dt
+        if self.current_radius <= self.radius:
+            self.current_radius += (self.radius * 10) * dt
